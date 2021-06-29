06552258df8b014b4fe36e2bffbd20eb1417d5c9
Kamil Stawik <kamil_stawik@op.pl>
2021-06-29 08:44:26 +0200

    code formating fixed

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ import Container from "./Container";
 
 function App() {
     const [hideDone, setHideDone] = useState(false);
-    const [tasks, setTasks] = useState([
-    ]);
+    const [tasks, setTasks] = useState([]);
 
     const toggleHideDone = () => {
         setHideDone(hideDone => !hideDone);

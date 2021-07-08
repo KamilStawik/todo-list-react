8c3ce2fa39b58f104117921202eb16367433f5dc
Kamil Stawik <kamil_stawik@op.pl>
2021-07-08 16:52:14 +0200

    styled-components added to Header and Container moduls

---
+++ src/Container/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+    max-width: 800px;
+    margin: 10px auto;
+    padding: 8px;
+`;
---
+++ src/Header/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.h1`
+    padding: 10px;
+    font-weight: 900;
+`;
